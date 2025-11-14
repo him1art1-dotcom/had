18b121527ef786156412a66bc7684b237c8e1167
him1art1-dotcom <him1.art1@gmail.com>
2025-11-14 11:39:17 +0300

    Delete js directory

--- js/school-config.js
+++
@@ -1,8 +0,0 @@
-window.HADER_SCHOOL_CONFIGS = {
-  "myschool": {
-    forceLogin: true,
-    forceLoginCreds: { user: "admin", pass: "admin" },
-    features: { remoteSync: false, reports: true }
-  },
-  "default": { forceLogin: false }
-};
