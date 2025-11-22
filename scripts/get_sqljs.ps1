8ea4b7024f73e1ca66be4fece2a18350b2fbf64b
him1art1-dotcom <him1.art1@gmail.com>
2025-11-22 11:21:05 +0300

    Delete scripts/get_sqljs.ps1

--- scripts/get_sqljs.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference='Stop'
-New-Item -ItemType Directory -Path .\lib -Force | Out-Null
-Invoke-WebRequest 'https://sql.js.org/dist/sql-wasm.js' -OutFile .\lib\sql-wasm.js
-Invoke-WebRequest 'https://sql.js.org/dist/sql-wasm.wasm' -OutFile .\lib\sql-wasm.wasm
